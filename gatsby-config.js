6ce01203fc8ec28705ba87bc4c5d9c4523d3a333
HammadAhmedBaloch <hammad.baloch0000@gmail.com>
2020-07-15 02:25:16 +0500

    final gatsby project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+    siteMetadata: {
+        title: 'The Innovators',
+        author: 'Hammad Ahmed'
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+                // spaceId: 'e2rnyl996iga',
+                // accessToken: 'WJLjzN6pcAaDIkoVIJn3U-ji0iGHc-gI_eFiaVGbojM'
+            }
+        },
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`
+            }
+        },
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    {
+                        resolve: 'gatsby-remark-images-contentful',
+                        options: {
+                            maxWidth: 750
+                        }
+                    }
+                ]
+            }
+        }
+    ]
 }
